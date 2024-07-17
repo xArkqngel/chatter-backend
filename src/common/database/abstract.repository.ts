876ea3680688ea36b548f9db3d9c9c3ac995e0a0
xArkqngel <arkqngel343@gmail.com>
2024-07-17 09:03:52 -0500

    Created Abstract repo

---
+++ src/common/database/abstract.repository.ts
@@ -0,0 +1,55 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (await createdDocument.save()).toJSON() as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.findOne(filterQuery).lean<TDocument>();
+    if (!document) {
+      this.logger.warn('Document not found:', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    updateQuery: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, updateQuery, {
+        new: true,
+      })
+      .lean<TDocument>();
+
+    if (!document) {
+      this.logger.warn('Document not found:', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>();
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return this.model.findOneAndDelete(filterQuery).lean<TDocument>();
+  }
+}
