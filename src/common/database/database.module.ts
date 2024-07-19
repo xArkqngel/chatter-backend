ee5d98b7fe01284c81e86749f988f5075f74457c
xArkqngel <arkqngel343@gmail.com>
2024-07-19 09:55:28 -0500

    database migrations\

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
+import { DBMigrationSerivce } from './db-migration.service';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ConfigService } from '@nestjs/config';
       inject: [ConfigService],
     }),
   ],
+  providers: [DBMigrationSerivce],
 })
 export class DatabaseModule {
   static forFeature(models: ModelDefinition[]) {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(Logger));
+  const configServide = app.get(ConfigService);
+  await app.listen(configServide.getOrThrow('PORT'));
 }
 bootstrap();
