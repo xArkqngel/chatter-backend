4e3eb5bd0a7961910232a8c2a77bad5d63b7d283
xArkqngel <arkqngel343@gmail.com>
2024-07-22 11:11:11 -0500

    Auth implemented correctly, using JWT, and Passport. Login working OK

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { AuthService } from './auth.service';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Response } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @UseGuards(LocalAuthGuard)
+  async login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.login(user, response);
+  }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: {
+          expiresIn: Number(configService.getOrThrow('JWT_EXPIRATION')),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
