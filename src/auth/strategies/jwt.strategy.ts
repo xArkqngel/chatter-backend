c2769e59be2b000c2a99f7c68a588b72d0ace92e
xArkqngel <arkqngel343@gmail.com>
2024-08-26 16:15:47 -0500

    jwt strategy implementation

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,13 +4,19 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TokenPayload } from '../token-payload.interface';
+import { getJwt } from '../jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req: Request) => req.cookies.Authentication,
+        (req: Request) => {
+          if (req.cookies.Authentication) {
+            return req.cookies.Authentication;
+          }
+          return getJwt(req.headers.authorization);
+        },
       ]),
       secretOrKey: configService.getOrThrow('JWT_SECRET'),
     });
