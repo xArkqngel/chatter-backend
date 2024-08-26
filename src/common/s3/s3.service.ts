45e93757ac5596619e42548f79fbc6ed3c1ad4c3
xArkqngel <arkqngel343@gmail.com>
2024-08-26 09:05:18 -0500

    add region to s3 for file upload

--- src/common/s3/s3.service.ts
+++ src/common/s3/s3.service.ts
@@ -1,23 +1,25 @@
-import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { PutObjectCommand, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
 import { FileUploadOptions } from './file-upload-options.interface';
 
 @Injectable()
 export class S3Service {
   private readonly client: S3Client;
 
-  constructor(private readonly configService: ConfigService) {
-    const accessKeyId = configService.get('AWS_ACCESS_KEY');
-    const secretAccessKey = configService.get('AWS_SECRET_ACCESS_KEY');
-    const clientConfig: S3ClientConfig = {};
+  constructor(configService: ConfigService) {
+    const accessKeyId = configService.get<string>('AWS_ACCESS_KEY');
+    const secretAccessKey = configService.get<string>('AWS_SECRET_ACCESS_KEY');
+    // Ensure you specify your bucket's region here
+    const region = configService.get<string>('AWS_REGION', 'us-east-1');
 
-    if (accessKeyId && secretAccessKey) {
-      clientConfig.credentials = {
+    const clientConfig: S3ClientConfig = {
+      region,
+      credentials: {
         accessKeyId,
         secretAccessKey,
-      };
-    }
+      },
+    };
 
     this.client = new S3Client(clientConfig);
   }
@@ -32,7 +34,7 @@ export class S3Service {
     );
   }
 
-  getOBjectUrl(bucket: string, key: string) {
+  getObjectUrl(bucket: string, key: string) {
     return `https://${bucket}.s3.amazonaws.com/${key}`;
   }
 }
